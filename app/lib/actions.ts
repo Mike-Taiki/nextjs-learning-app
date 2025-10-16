529a7ea3aac4f69f88bdb3e3615e8460475dd555
Michael gomes <michael.ferreira@poatek.com>
2025-10-16 15:41:41 -0300

    feat: add update customer validation

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -18,9 +18,13 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 const FormSchema = z.object({
 	id: z.string(),
-	customerId: z.string({
-		invalid_type_error: "Please select a customer",
-	}),
+	customerId: z
+		.string({
+			invalid_type_error: "Please select a customer",
+		})
+		.nonempty({
+			message: "Please select a customer",
+		}),
 	amount: z.coerce
 		.number()
 		.gt(0, { message: "Please enter an amount greater than $0" }),
@@ -67,16 +71,29 @@ export async function createInvoice(prevState: State, formData: FormData) {
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function updateInvoice(id: string, formData: FormData) {
+export async function updateInvoice(
+	id: string,
+	prevState: State,
+	formData: FormData
+) {
+	const validatedFields = UpdateInvoice.safeParse({
+		customerId: formData.get("customerId"),
+		amount: formData.get("amount"),
+		status: formData.get("status"),
+	});
+
+	if (!validatedFields.success) {
+		console.log("validatedFields: ", validatedFields.error.flatten());
+		return {
+			errors: validatedFields.error.flatten().fieldErrors,
+			message: "Missing Fields. Failed to Edit Invoice.",
+		};
+	}
+
+	const { customerId, amount, status } = validatedFields.data;
+	const amountInCents = amount * 100;
+
 	try {
-		const { customerId, amount, status } = UpdateInvoice.parse({
-			customerId: formData.get("customerId"),
-			amount: formData.get("amount"),
-			status: formData.get("status"),
-		});
-
-		const amountInCents = amount * 100;
-
 		await sql`
 			UPDATE invoices
 			SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
@@ -94,11 +111,9 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-	throw new Error("Failed to Delete Invoice");
 	try {
 		await sql`DELETE FROM invoices WHERE id = ${id}`;
 	} catch (error) {
-		console.error(error);
 		return {
 			message: "Database Error: Failed to Delete Invoice.",
 		};
